04a74b0da5059acd0372a069515935936ef37e09
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-09-23 22:50:36 +0200

    Fin ControleSaisie

--- 02-Algorithmes/ControleSaisie/Program.cs
+++ 02-Algorithmes/ControleSaisie/Program.cs
@@ -6,7 +6,38 @@ namespace ControleSaisie
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello world !");
+            /* VARIABLES */
+
+            string prenomUtilisateur = " ";
+
+            /* DEBUT PROGRAMME */
+
+            //Boucle tant que prénom ne contient pas au moins 2 caractères
+            while (prenomUtilisateur.Length < 2)
+            {
+                try
+                {
+                    System.Console.Write("Entrer votre pénom (2 lettres minimum): ");
+                    prenomUtilisateur = Console.ReadLine();
+
+                    //Vérification pas d'alphanumérique dans le prénom
+                    if(prenomUtilisateur.Any(char.IsDigit))
+                    {
+                        System.Console.WriteLine("Attention, le prénom ne doit pas contenir de chiffres");
+                        prenomUtilisateur = " ";
+                    }
+                }
+                catch
+                {
+                    System.Console.WriteLine("ERREUR : prénom invalide.");
+                }
+                
+            }
+
+            //Quand le prénom convient on affiche le message suivi du prénom
+            Console.WriteLine("Bonjour " + prenomUtilisateur);
+
+            /* FIN PROGRAMME */
         }
     }
 }
