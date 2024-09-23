4bb30d2bba561bbe98f3cec9127409e6c4065762
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-09-23 14:02:03 +0200

    Fin MajeurTest

--- 01-Algorithmes/MajeurTest/Program.cs
+++ 01-Algorithmes/MajeurTest/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace MajeurTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /* VARIABLES */
+
+            int age;
+
+            /* DEBUT PROGRAMME */
+
+            Console.Write("QUel est votre âge ? : ");
+            age = int.Parse(Console.ReadLine());
+
+            if(age < 0)
+            {
+                Console.WriteLine("Vous n'êtes pas né");
+            }
+            else if(age >= 18)
+            {
+                Console.WriteLine("Vous êtes majeur.");
+            }
+            else
+            {
+                Console.WriteLine("Vous êtes mineur.");
+            }
+            /* FIN PROGRAMME */
+        }
+    }
+}
