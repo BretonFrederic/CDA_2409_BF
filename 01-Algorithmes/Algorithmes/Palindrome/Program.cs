938933b1b8c0000f9f8537818e72a87a06c41934
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-09-26 09:56:47 +0200

    Fin Palindrome

--- 01-Algorithmes/Algorithmes/Palindrome/Program.cs
+++ 01-Algorithmes/Algorithmes/Palindrome/Program.cs
@@ -3,6 +3,7 @@ using System.Reflection.Emit;
 using System.Reflection.PortableExecutable;
 using System.Text;
 using System.Threading.Channels;
+using System.Linq;
 
 namespace Palindrome
 {
@@ -32,13 +33,31 @@ namespace Palindrome
         static void Main(string[] args)
         {
             /* VARIABLES */
-
+            List<string> mots;
+            bool estPalindrome;
 
             /* TRAITEMENT */
+            estPalindrome = false;
+            // Inverser le mot et stocker dans un new string
+            mots = new List<string>(){ "LONDRES", "" };
+            mots[1] = new string(mots[0].Reverse().ToArray());
 
+            // Comparer le mot avec sa copie inversée. Définir si c'est un palindrome avec une booléen
+            if (mots[0] == mots[1])
+            {
+                estPalindrome = true;
+            }
 
             /* AFFICHAGE */
-            Console.WriteLine("Hello, World!");
+
+            if (estPalindrome)
+            {
+                Console.WriteLine($"Le mot {mots[0]} inversé est {mots[1]} c'est un palindrome.");
+            }
+            else
+            {
+                Console.WriteLine($"Le mot {mots[0]} inversé est {mots[1]} ce n'est pas un palindrome.");
+            }
         }
     }
 }
