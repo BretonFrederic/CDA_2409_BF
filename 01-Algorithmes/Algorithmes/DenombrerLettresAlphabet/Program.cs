da8927c1f7b0d695a9df46329e4be7b33e123e6c
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-09-30 16:49:46 +0200

    Création DenombrerLettresAlphabet

---
+++ 01-Algorithmes/Algorithmes/DenombrerLettresAlphabet/Program.cs
@@ -0,0 +1,34 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Text;
+
+namespace DenombrerLettresAlphabet
+{
+    internal class Program
+    {
+        /* CONSIGNE */
+
+        // Exercice 6.4 : DENOMBRER LES LETTRES DE L’ALPHABET
+
+        // Lire une chaine de caractères d’au moins 100 caractères(à contrôler).
+        // Compter et afficher:
+
+        //    le nombre de consonnes.
+        //    le nombre de voyelles
+        //    le nombre de chiffres
+        //    la moyenne des chiffres, 0 si aucun chiffre.
+
+        /* FONCTIONS */
+
+        static void Main(string[] args)
+        {
+            /* VARIABLES */
+            string chaineCharacteres = "";
+
+            /* TRAITEMENT */
+
+
+            /* AFFICHAGE */
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
