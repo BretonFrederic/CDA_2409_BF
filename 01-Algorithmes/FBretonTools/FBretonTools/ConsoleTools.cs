6dc729df0adc0b226eaa80c9e925b9bb05c3d0f6
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-10-08 21:03:28 +0200

    Ajout fonction mot de passe dans FBretonTools

--- 01-Algorithmes/FBretonTools/FBretonTools/ConsoleTools.cs
+++ 01-Algorithmes/FBretonTools/FBretonTools/ConsoleTools.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
 
 namespace FBretonTools
 {
@@ -53,6 +54,35 @@ namespace FBretonTools
 
             } while (!saisieOk);
             return valeurRetour;
+        }
+
+        /*
+        L'utilisateur entre un mot de passe
+        Le programme contrôle si le mot de passe respecte les règles en vigueur
+        - 12 caractères minimum
+            ET Au moins 1 minuscule
+            ET Au moins 1 majuscule
+            ET Au moins 1 chiffre
+            ET Au moins 1 caractère spécial
+        OU
+        - 20 caractères minimum
+            ET Au moins 1 minuscule
+            ET Au moins 1 majuscule
+            ET Au moins 1 chiffre
+        */
+        public static string DemanderMotDePasse(string _question = "Entrer mot de passe : ")
+        {
+            string motDePasse;
+            string formatMotDePasse = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{12,}$|^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{20,}$";
+
+            do
+            {
+                Console.WriteLine(_question);
+                motDePasse = Console.ReadLine() ?? "";
+            } while (!Regex.IsMatch(motDePasse, formatMotDePasse, RegexOptions.IgnoreCase));
+
+            // Console.WriteLine("Mot de passe valide : " + motDePasse);
+            return motDePasse;
         }
     }
 }
