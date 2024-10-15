5177932de15d799d78e37c8dfde52762f30d4f9c
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-10-15 13:01:04 +0200

    Gestion cas date invalide age inférieur à 0

--- 01-Algorithmes/Algorithmes/ListUtilisateurs/Program.cs
+++ 01-Algorithmes/Algorithmes/ListUtilisateurs/Program.cs
@@ -86,14 +86,19 @@ namespace ListUtilisateurs
                     // 3. Le programme calcule l'âge de la personne en cours d'ajout.
                     TimeSpan interval = dateActuelle - naissanceDateTime;
                     age = (int)(interval.Days / 365.25);
-                    
+
+
+                    if (age <= 0)
+                    {
+                        Console.WriteLine("Date invalide.");
+                    }
                     // -Si l’âge est supérieur ou égal à 18(majeur) Le programme demande à l'utilisateur de saisir son métier.
-                    if (age >= 18)
+                    else if (age >= 18)
                     {
                         metierCouleur = DemanderInfoUtilisateur("Saisir un métier : ");
                     }
                     // - Si l’âge est inférieur à 18(mineur) Le programme demande à l'utilisateur de saisir sa couleur préférée.
-                    else
+                    else if(age > 0 || age < 18)
                     {
                         metierCouleur = DemanderInfoUtilisateur("Saisir une couleur préférée : ");
                     }
@@ -114,10 +119,14 @@ namespace ListUtilisateurs
             // - Afficher tous les utilisateurs enregistrés en respectant ce format : -Nom Prénom - Date de naissance(âge) -Métier / Couleur préférée
             Console.WriteLine();
             Console.WriteLine("Liste des personnes enregistrées : ");
-            for (int i = 0; i < listeUtilisateurs.Count; i++)
+            if(age > 0)
             {
-                Console.WriteLine("- " + listeUtilisateurs[i][0] + " - " + listeUtilisateurs[i][1] + " ans" + " - " + listeUtilisateurs[i][2]);
+                for (int i = 0; i < listeUtilisateurs.Count; i++)
+                {
+                    Console.WriteLine("- " + listeUtilisateurs[i][0] + " - " + listeUtilisateurs[i][1] + " ans" + " - " + listeUtilisateurs[i][2]);
+                }
             }
+            
 
             // 6.Le programme remercie l'utilisateur et se termine
             Console.WriteLine();
