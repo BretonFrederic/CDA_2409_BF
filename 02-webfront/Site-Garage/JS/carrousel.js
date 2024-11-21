4e01ecdf0e1649b06e7af6fa2bb23dfe32922d59
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-11-21 14:47:09 +0100

    Site-Garage Début slider

--- 02-webfront/Site-Garage/JS/carrousel.js
+++ 02-webfront/Site-Garage/JS/carrousel.js
@@ -1,27 +1,26 @@
-let previewSlide = document.querySelector('#preview-slide');
-let activeSlide = document.querySelector('#active-slide');
-let nextSlide = document.querySelector('#next-slide');
+let slide1 = document.querySelector('#slide-1');
+let slide2 = document.querySelector('#slide-2');
+let slide3 = document.querySelector('#slide-3');
 const slides = ['automobile-2377221_1280.jpg', 'automobile-3102299_1280.jpg', 'automobile-3345646_1280.jpg', 
                 'car-139529_1280.jpg', 'car-5667103_1280.jpg', 'car-6483723_1280.jpg', 'convertible-1630448_1280.jpg', 
                 'ford-1730888_1280.jpg', 'ford-mustang-4510677_1280.jpg', 'ford-mustang-4765055_1280.jpg'];
 
-previewSlide.style.backgroundImage= "url(./images/carrousel/"+slides[0]+")";
-activeSlide.style.backgroundImage= "url(./images/carrousel/"+slides[1]+")";
-nextSlide.style.backgroundImage= "url(./images/carrousel/"+slides[2]+")";
+slide1.style.backgroundImage= "url(./images/carrousel/"+slides[0]+")";
+slide2.style.backgroundImage= "url(./images/carrousel/"+slides[1]+")";
+slide3.style.backgroundImage= "url(./images/carrousel/"+slides[2]+")";
 
-setInterval("AutoSlide()", 2000);
-setInterval("InitSlide()", 4000);
+let move = setInterval("AutoSlide()", 2000);
 
 function AutoSlide(){
-    previewSlide.style.left="200%";
-    activeSlide.style.left="100%";
-    nextSlide.style.left="0%";
-    console.log(getInterval);
+    slide1.style.left="0%";
+    slide2.style.left="100%";
+    slide3.style.left="200%";
+    console.log(move);
+    move++;
+    if(move >= 5){
+        slide3.style.left="-100%";
+        clearInterval("move");    
+    }
 }
-
-function InitSlide(){
-    previewSlide.style.left="-100%";
-}
-
 
 
