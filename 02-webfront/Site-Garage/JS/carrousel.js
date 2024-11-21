5bcf64b535f3a4deddb9c9fd9cf9e643775b1660
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-11-22 00:55:41 +0100

    Site Garage slider

--- 02-webfront/Site-Garage/JS/carrousel.js
+++ 02-webfront/Site-Garage/JS/carrousel.js
@@ -1,26 +1,39 @@
-let slide1 = document.querySelector('#slide-1');
-let slide2 = document.querySelector('#slide-2');
-let slide3 = document.querySelector('#slide-3');
+let position = document.querySelectorAll('.slide');
+
 const slides = ['automobile-2377221_1280.jpg', 'automobile-3102299_1280.jpg', 'automobile-3345646_1280.jpg', 
                 'car-139529_1280.jpg', 'car-5667103_1280.jpg', 'car-6483723_1280.jpg', 'convertible-1630448_1280.jpg', 
                 'ford-1730888_1280.jpg', 'ford-mustang-4510677_1280.jpg', 'ford-mustang-4765055_1280.jpg'];
 
-slide1.style.backgroundImage= "url(./images/carrousel/"+slides[0]+")";
-slide2.style.backgroundImage= "url(./images/carrousel/"+slides[1]+")";
-slide3.style.backgroundImage= "url(./images/carrousel/"+slides[2]+")";
+position[0].style.backgroundImage = "url(./images/carrousel/"+slides[0]+")";
+position[1].style.backgroundImage = "url(./images/carrousel/"+slides[1]+")";
+position[2].style.backgroundImage = "url(./images/carrousel/"+slides[2]+")";
 
-let move = setInterval("AutoSlide()", 2000);
+const tab = [["0%", "100%", "200%"], ["0%", "100%", "-100%"], ["100%", "200%", "0%"], ["100%", "-100%", "0%"], ["200%", "0%", "100%"], ["-100%", "0%", "100%"]];
+
+let index = 0;
+let moveNext = setInterval(AutoSlide, 1000);
 
 function AutoSlide(){
-    slide1.style.left="0%";
-    slide2.style.left="100%";
-    slide3.style.left="200%";
-    console.log(move);
-    move++;
-    if(move >= 5){
-        slide3.style.left="-100%";
-        clearInterval("move");    
+    moveNext = moveNext + 1;
+    if(moveNext > 3){
+        moveNext = 0;
+        index++;
+        if(index > 5){
+            index = 0;
+        }
+    }
+    for(let i = 0; i < 3; i++){
+        position[i].style.left = tab[index][i];
+        if(position[i].style.left === "200%"){
+            position[i].style.zIndex = -99;
+        }
+        else{
+            position[i].style.zIndex = 99999;
+        }
     }
 }
+
+
+
 
 
