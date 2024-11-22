a3f5d1299712f6e80874e82c1be708fedab8887f
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2024-11-22 07:31:08 +0100

    Site Garage slider

--- 02-webfront/Site-Garage/JS/carrousel.js
+++ 02-webfront/Site-Garage/JS/carrousel.js
@@ -1,8 +1,8 @@
 let position = document.querySelectorAll('.slide');
 
-const slides = ['automobile-2377221_1280.jpg', 'automobile-3102299_1280.jpg', 'automobile-3345646_1280.jpg', 
-                'car-139529_1280.jpg', 'car-5667103_1280.jpg', 'car-6483723_1280.jpg', 'convertible-1630448_1280.jpg', 
-                'ford-1730888_1280.jpg', 'ford-mustang-4510677_1280.jpg', 'ford-mustang-4765055_1280.jpg'];
+const slides = ['car-01.jpg', 'car-02.jpg', 'car-03.jpg', 
+                'car-04.jpg', 'car-05.jpg', 'car-06.jpg', 'car-07.jpg', 
+                'car-08.jpg', 'car-09.jpg', 'car-10.jpg', 'car-11.jpg', 'car-12.jpg', 'car-13.jpg'];
 
 position[0].style.backgroundImage = "url(./images/carrousel/"+slides[0]+")";
 position[1].style.backgroundImage = "url(./images/carrousel/"+slides[1]+")";
@@ -11,6 +11,8 @@ position[2].style.backgroundImage = "url(./images/carrousel/"+slides[2]+")";
 const tab = [["0%", "100%", "200%"], ["0%", "100%", "-100%"], ["100%", "200%", "0%"], ["100%", "-100%", "0%"], ["200%", "0%", "100%"], ["-100%", "0%", "100%"]];
 
 let index = 0;
+let newIndex = 0;
+let imgNum = 0;
 let moveNext = setInterval(AutoSlide, 1000);
 
 function AutoSlide(){
@@ -26,6 +28,12 @@ function AutoSlide(){
         position[i].style.left = tab[index][i];
         if(position[i].style.left === "200%"){
             position[i].style.zIndex = -99;
+            imgNum = slides.length-newIndex;
+            position[i].style.backgroundImage = "url(./images/carrousel/"+slides[imgNum]+")";
+            newIndex++;
+            if(newIndex > 12){
+                newIndex = 0;
+            }
         }
         else{
             position[i].style.zIndex = 99999;
