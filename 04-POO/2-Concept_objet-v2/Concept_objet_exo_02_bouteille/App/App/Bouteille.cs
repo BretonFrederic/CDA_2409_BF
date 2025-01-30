c440103def2135f309fbd66047d746d3207c757b
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-01-30 16:58:57 +0100

    Concept_objet_exo_02_bouteille App CSharp

---
+++ 04-POO/2-Concept_objet-v2/Concept_objet_exo_02_bouteille/App/App/Bouteille.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    internal class Bouteille
+    {
+        // CHAMPS
+        public string? marque { private set; get; }
+        public string? typeContenu { private set; get; }
+        public double volume { private set; get; }
+        public double quantite { private set; get; }
+        public bool estOuverte { private set; get; }
+
+        // CONSTRUCTEUR PAR DEFAUT
+        public Bouteille()
+        {
+            this.marque = "inconnu";
+            this.typeContenu = "eau";
+            this.volume = 1.0;
+            this.quantite = 0.0;
+            this.estOuverte = false;
+        }
+
+        // CONSTRUCTEUR
+        public Bouteille(string _marque, string _typeContenu, double _volume, double _quantite)
+        {
+            this.marque = _marque;
+            this.typeContenu = _typeContenu;
+            this.volume = _volume;
+            this.quantite = _quantite;
+            this.estOuverte = false;
+        }
+
+        // METHODES
+        public bool Ouvrir()
+        {
+            if (!this.estOuverte)
+            {
+                // je peux ouvrir la bouteille si elle est fermée
+                this.estOuverte = true;
+                // l'action ouvrir a été réalisé
+                return true;
+            }
+            else
+            {
+                // bouteille déjà ouverte je pas pas l'ouvrir
+                // l'action ouvrir n'a pas été réalisé
+                return false;
+            }
+        }
+
+        public bool Fermer()
+        {
+            if (this.estOuverte)
+            {
+                // je peux fermer la bouteille si elle est ouverte
+                this.estOuverte = false;
+                // l'action fermer a été réalisé
+                return true;
+            }
+            else
+            {
+                // bouteille déjà fermée je pas pas la fermer
+                // l'action fermer n'a pas été réalisé
+                return false;
+            }
+        }
+        public bool Vider()
+        {
+            if (this.estOuverte && this.quantite > 0)
+            {
+                // Bouteille ouverte on vide la bouteille
+                this.quantite = 0.0;
+                // l'action vider a été réalisé
+                return true;
+            }
+            else
+            {
+                // bouteille fermée impossible de vider
+                // l'action vider n'a pas été réalisé
+                return false;
+            }
+        }
+        public bool Remplir()
+        {
+            if (this.estOuverte && this.quantite < this.volume)
+            {
+                // Bouteille ouverte on rempli la bouteille
+                this.quantite = this.volume;
+                // l'action remplir a été réalisé
+                return true;
+            }
+            else
+            {
+                // bouteille fermée impossible de remplir
+                // l'action remplir n'a pas été réalisé
+                return false;
+            }
+        }
+    }
+
+}
---
+++ 04-POO/2-Concept_objet-v2/Concept_objet_exo_02_bouteille/App/App/Program.cs
@@ -0,0 +1,95 @@
+﻿using System.Linq.Expressions;
+
+namespace App
+{
+    internal class Program
+    {
+        // Choisir la quantité à remplir ou vider
+        static public double ChoisirQuantite()
+        {
+            double quantite = 0;
+            Console.WriteLine("Choisir la quantité en litre : ");
+            if(int.TryParse(Console.ReadLine(), out quantite){
+                //
+            }
+            return 0.0;
+        }
+        static void Main(string[] args)
+        {
+            char quitter = ' ';
+
+            Bouteille BouteilleVittel = new Bouteille("Vittel", "eau", 1.5, 1.5);
+
+            do
+            {
+                int menu = 0;
+                while (menu < 1 || menu > 4)
+                {
+                    Console.WriteLine("\nChosir :\n\t1 - Ouvrir la bouteille" +
+                                              "\n\t2 - Fermer la bouteille" +
+                                              "\n\t3 - Vider la bouteille" +
+                                              "\n\t4 - Remplir la bouteille");
+                    try
+                    {
+                        menu = int.Parse(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex);
+                    }
+
+                }
+
+                switch (menu)
+                {
+                    case 1:
+                        if (BouteilleVittel.Ouvrir())
+                        {
+                            Console.WriteLine("La bouteille a été ouverte.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("La bouteille est déjà ouverte.");
+                        }
+                        break;
+                    case 2:
+                        if (BouteilleVittel.Fermer())
+                        {
+                            Console.WriteLine("La bouteille a été fermée.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("La bouteille est déjà fermée.");
+                        }
+                        break;
+                    case 3:
+                        if (BouteilleVittel.Vider())
+                        {
+                            Console.WriteLine("La bouteille a été vidée.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("La bouteille est fermée ou déjà vide.");
+                        }
+                        break;
+                    case 4:
+                        if (BouteilleVittel.Remplir())
+                        {
+                            Console.WriteLine("La bouteille a été remplie.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("La bouteille est fermée ou déjà remplie au maximum de sa capacité.");
+                        }
+                        break;
+                    default:
+                        //
+                        break;
+                }
+
+                Console.Write("Entrer q pour quitter ou n'importe quelle autre pour continuer :");
+            } while (!Console.ReadKey().Key.ToString().Equals("q", StringComparison.OrdinalIgnoreCase));
+
+        }
+    }
+}
