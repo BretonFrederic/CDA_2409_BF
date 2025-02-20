6bc636da5dce0f347a463df64698d8cbaf6ca58b
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-02-20 17:08:04 +0100

    Lepidoptere

---
+++ 04-POO/2-Concept_objet-v2/exo_19_Lepidoptere/Lepidoptere/ClassLibraryLepidoptere/Lepidoptere.cs
@@ -0,0 +1,98 @@
+﻿namespace ClassLibraryLepidoptere
+{
+    public abstract class StadeEvolution
+    {
+        public abstract StadeEvolution SeMetamorphoser();
+        public abstract void SeDeplacer();
+    }
+
+    public class Oeuf :StadeEvolution
+    {
+        public Oeuf()
+        {
+
+        }
+        public override StadeEvolution SeMetamorphoser()
+        {
+            return new Chenille();
+        }
+        public override void SeDeplacer()
+        {
+            Console.WriteLine("Je peux pas !");
+        }
+
+    }
+
+    public class Chenille : StadeEvolution
+    {
+        public Chenille()
+        {
+
+        }
+        public override StadeEvolution SeMetamorphoser()
+        {
+            return new Chrysalide();
+        }
+        public override void SeDeplacer()
+        {
+            Console.WriteLine("Je peux ramper !");
+        }
+
+    }
+
+    public class Chrysalide : StadeEvolution
+    {
+        public Chrysalide()
+        {
+
+        }
+        public override StadeEvolution SeMetamorphoser()
+        {
+            return new Papillon();
+        }
+        public override void SeDeplacer()
+        {
+            Console.WriteLine("Je peux pas je suis en mutation !");
+        }
+
+    }
+
+    public class Papillon : StadeEvolution
+    {
+        public Papillon()
+        {
+
+        }
+        public override StadeEvolution SeMetamorphoser()
+        {
+            return new Chenille();
+        }
+        public override void SeDeplacer()
+        {
+            Console.WriteLine("Je peux voler !");
+        }
+
+    }
+    public class Lepidoptere
+    {
+        private string espece;
+        private StadeEvolution? sonStadeCourant;
+
+        // Constructeur
+        public Lepidoptere(string espece)
+        {
+            this.espece = espece;
+            this.sonStadeCourant = new Oeuf();
+        }
+
+        // Méthodes
+        public void SeMetamorphoser()
+        {
+            this.sonStadeCourant.SeMetamorphoser();
+        }
+        public void SeDeplacer()
+        {
+            this.sonStadeCourant.SeDeplacer();
+        }
+    }
+}
