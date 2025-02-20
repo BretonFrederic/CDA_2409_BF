c40e0877b6af92853490308df8a689f349dc2cc6
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-02-20 17:52:50 +0100

    Lepidoptere Une Classe un fichier

---
+++ 04-POO/2-Concept_objet-v2/exo_19_Lepidoptere/Lepidoptere/ClassLibraryLepidoptere/StadeEvolution.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryLepidoptere
+{
+    public abstract class StadeEvolution
+    {
+        public abstract StadeEvolution SeMetamorphoser();
+        public abstract void SeDeplacer();
+    }
+}
