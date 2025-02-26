c04de55de62bb87626f480959209c16b62d9ee02
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-02-26 15:55:55 +0100

    exo 04-introDate

--- 02-webfront/01-Javascript/04-introDates/script.js
+++ 02-webfront/01-Javascript/04-introDates/script.js
@@ -1,6 +1,13 @@
-var dateNaissance = document.querySelectorAll("name");
-var dateActuelle = new Date();
+const inputDate = document.querySelector("#date").value;
+const dateAnniversaire = inputDate;
+let resultat = document.querySelector("#resultat");
+let btnValider = document.querySelector("#btn-valider");
+const dateActuelle = new Date();
 
-document.write("Vous êtes néé le : ");
-document.write(dateActuelle.getDate());
+// document.write("Vous êtes néé le : ");
+// document.write(dateActuelle.getDate());
 
+btnValider.addEventListener('click', function(){
+    console.log(dateAnniversaire);
+    console.log(dateActuelle.getTime());
+});
