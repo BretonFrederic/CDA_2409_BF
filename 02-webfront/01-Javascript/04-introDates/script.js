24bc585a25715e957938e2e457fa0ed6d1814586
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-02-27 16:59:33 +0100

    exo 04-introDate signe astrologique

--- 02-webfront/01-Javascript/04-introDates/script.js
+++ 02-webfront/01-Javascript/04-introDates/script.js
@@ -12,7 +12,7 @@ btnCalculer.addEventListener('click', function(){
         const d2 = new Date(dateUtilisateur);
 
         // Calculer la difference entre la dateActuelle et la dateAnniversaire
-        const dateDiff = d1.getTime() - d2.getTime();
+        const dateDiff = (d1.getTime() - d2.getTime());
         if(dateDiff < 0){
             resultat.innerHTML = "Erreur la date renseignée doit être dans le passé.";
             console.log("Erreur la date renseignée doit être dans le passé.");
@@ -22,12 +22,43 @@ btnCalculer.addEventListener('click', function(){
             // Millisecondes/1000 -> secondes | secondes/60 -> minutes | minutes/60 -> heures | heures/24 -> jours | jours/365,25 -> années
             const anneeDiff = ((((dateDiff/1000)/60)/60)/24)/365.25;
 
+
             resultat.innerHTML = "Vous êtes né le : <span class='color-blue-bold'>" + d2.toLocaleDateString("fr") + "</span> à <span class='color-blue-bold'>" + d2.toLocaleTimeString() +
             ".</span><br><br>Il s'est écoulé " + Math.floor(anneeDiff) + " année(s) depuis votre naissance";
             console.log("Vous êtes né le : " + d2.toLocaleDateString("fr") + " à " + d2.toLocaleTimeString());
 
             // Math.floor arrondir années complètes (-5.1 -> -6 / 10.60 -> 10)
             console.log("Il s'est écoulé " + Math.floor(anneeDiff) + " depuis votre naissance");
+
+            // Selectionner le signe astrologique
+            //-------Capricorne----------------
+            if(d2.getMonth()==0){
+                if(d2.getDate() <= 19){
+                    console.log("Capricorne");
+                }
+            //-------Verseau----------------
+                else if(d2.getDate() >=20){
+                    console.log("Verseau");
+                }
+            }
+            else if(d2.getMonth()==1){
+                if(d2.getDate() <= 18){
+                    console.log("Verseau");
+                }
+            //-------Poissons----------------
+                else if(d2.getDate() >= 19){
+                    console.log("Poissons");
+                }
+            }
+            else if(d2.getMonth()==2){
+                if(d2.getDate() <= 20){
+                    console.log("Poissons");
+                }
+            //-------Bélier----------------
+                else if(d2.getDate() >= 21){
+                    console.log("Bélier");
+                }
+            }
 
             // Ajouter une balise p pour afficher le signe astrologique
             let astroResult = document.createElement('p');
