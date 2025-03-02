165ce5c6ccedf4e8ef778a5013f0ec629a82bb09
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-03-02 19:22:03 +0100

    exo js 05-IntervallesTemporelles Afficher date du jour

--- 02-webfront/01-Javascript/05-intervallesTemporelles/script.js
+++ 02-webfront/01-Javascript/05-intervallesTemporelles/script.js
@@ -1,21 +1,20 @@
-const btnAfficher = document.querySelector("#btn-afficher");
-const container = document.querySelector(".container");
-const resultatDate = document.querySelector("#resultat-date");
+const btnAfficherDate = document.querySelector("#btn-afficher");
 
-btnAfficher.addEventListener('click', function(){
-    // Recuperer les inputs date et heure
-    const dateActuelle = document.querySelector("#date").value;
-    const timeActuelle = document.querySelector("#time").value;
+btnAfficherDate.addEventListener('click', function(){
+    let dateDuJour = new Date().toISOString().split('T')[0];
+    let timeFormat = new Intl.DateTimeFormat("fr-FR", {hour:"2-digit", minute:"2-digit", second:"2-digit"});
 
-    if(dateActuelle != "" && timeActuelle != ""){
-        resultatDate.innerHTML = "Aujourd'hui nous sommes le <span class='color-blue-bold'>" + 
-        new Date(dateActuelle).toLocaleDateString('fr') + 
-        "</span>, l'heure courante est : <span class='color-blue-bold'>" + 
-        timeActuelle + "</span>";
-    }
-    else{
-        resultatDate.textContent = "Compléter la date et l'heure."
-    }
+    let heureDuJour = timeFormat.format(Date.now());
+    document.querySelector("#date").value = dateDuJour;
+    document.querySelector("#time").value = heureDuJour;
 
-    
+    let dateFormat = new Intl.DateTimeFormat("fr-FR", {dateStyle:"short"});
+    dateDuJour = dateFormat.format(Date.now());
+
+    document.querySelector("#resultat-date").innerHTML = "Aujourd'hui nous sommes le <span class='color-blue-bold'>" + 
+    dateDuJour + "</span>, l'heure courante est : <span class='color-blue-bold'>" + heureDuJour + "</span>.";
+
+    document.querySelector("#resultat-date").setAttribute("style", "border-top:2px solid black;" +
+                                                          "border-bottom:2px solid black;" +
+                                                          "padding: 16px 0px;");
 });
