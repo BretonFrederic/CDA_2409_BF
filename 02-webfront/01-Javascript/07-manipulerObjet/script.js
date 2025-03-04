66a870c1a6013a48767140c04ec733cc81046c11
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-03-04 21:14:14 +0100

    exo js 07-manipulerObjet Formulaire pré-rempli

--- 02-webfront/01-Javascript/07-manipulerObjet/script.js
+++ 02-webfront/01-Javascript/07-manipulerObjet/script.js
@@ -4,12 +4,12 @@ const myEmployee = {
     birthday: "1981-11-12", 
     salary: 2150
 }
-const principal = document.querySelector("#principal");
+const dataProfil = document.querySelector("#data-profil");
 
 // Créer une table
 const myTable = document.createElement('table');
-myTable.setAttribute("style", "margin-top:20px; border-collapse: collapse;");
-principal.appendChild(myTable);
+myTable.setAttribute("style", "margin-top:20px; margin-bottom:30px; border-collapse: collapse;");
+dataProfil.appendChild(myTable);
 
 // Créer la balise thead (dans la table)
 const myTHeader = myTable.createTHead();
@@ -57,8 +57,44 @@ function addCell(row, text){
     row.appendChild(myCell);
 }
 
+// Renvoyer un tableau contenant les valeurs d'un objet
+const employeeArray = Object.values(myEmployee);
+
+// Récupérer firstname et lastname de l'objet
+const firstname = myEmployee.firstname;
+const lastname = myEmployee.lastname;
+
+// Générer l'email
+const eMail = firstname+"."+lastname+"@example.com";
+
+// Ajout de l'email dans le tableau
+employeeArray.splice(3, 0, eMail.toLowerCase());
+
+// Ajout du symbole monétaire pour salary
+const salary = myEmployee.salary + " €";
+employeeArray.splice(4, 1, salary);
+
 // Ajouter cellule et son texte pour chaque cellule du tbody
-const info = ["Nom", "Prénom", "Date de naissance", "Email", "Salaire"];
-info.forEach(element => {
+console.log(employeeArray);
+employeeArray.forEach(element => {
     addCell(myRow, element);
 });
+
+// Pré-remplir le formulaire de profil
+const inputProfil = document.querySelectorAll(".input-profil");
+const infoProfil = Object.values(myEmployee);
+
+for (let index = 0; index < infoProfil.length; index++) {
+    const firstN = myEmployee.firstname;
+    const lastN = myEmployee.lastname;
+    infoProfil.splice(0, 1, firstN);
+    infoProfil.splice(1, 1, lastN);
+    inputProfil[index].value = infoProfil[index];
+    
+}
+
+// Contrôler les valeurs saisies dans le formulaire
+
+// générer le nouveau email
+
+// Enregistrer les nouvelles valeurs dans le tableau
