1f7e9e333a0c18b6402eef1d52a86d4c090271ea
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-03-05 16:59:06 +0100

    exo 07-manipulerObjet modifier profil js

--- 02-webfront/01-Javascript/07-manipulerObjet/script.js
+++ 02-webfront/01-Javascript/07-manipulerObjet/script.js
@@ -6,7 +6,7 @@ const myEmployee = {
 }
 const dataProfil = document.querySelector("#data-profil");
 
-// Créer une table
+// Créer une table -------------------------------------------------------------------------------------------
 const myTable = document.createElement('table');
 myTable.setAttribute("style", "margin-top:20px; margin-bottom:30px; border-collapse: collapse;");
 dataProfil.appendChild(myTable);
@@ -16,12 +16,6 @@ const myTHeader = myTable.createTHead();
 
 // Créer le tr (dans le thead)
 const myTitleRow = myTHeader.insertRow();
-
-// Créer le th (dans le tr du thead)
-// const myCellTitle = document.createElement('th');
-// myCellTitle.setAttribute("style", "border:2px solid #e3fae3; padding:5px; background-color:#90ee90");
-// myCellTitle.textContent = "Nom";
-// myTitleRow.appendChild(myCellTitle);
 
 // Fonction pour créer les cellules titres du tableau
 function addCellTitle(row, text){
@@ -41,13 +35,7 @@ myTitle.forEach(element => {
 const myTBody = myTable.createTBody();
 
 // Créer le tr (dans le tbody)
-const myRow = myTBody.insertRow();
-
-// Créer le td (dans le tr du tbody)
-// const myCell = document.createElement('td');
-// myCell.setAttribute("style", "border:2px solid #e3fae3; padding:5px; background-color:#90ee90");
-// myCell.textContent = "info";
-// myRow.appendChild(myCell);
+let myRow = myTBody.insertRow();
 
 // Fonction pour créer les cellules dans tbody du tableau
 function addCell(row, text){
@@ -57,15 +45,22 @@ function addCell(row, text){
     row.appendChild(myCell);
 }
 
+// Générer l'email
+function createMail(fName, lName){
+    const eMail = fName+"."+lName+"@example.com";
+    return eMail;
+}
+
 // Renvoyer un tableau contenant les valeurs d'un objet
-const employeeArray = Object.values(myEmployee);
+let employeeArray = Object.values(myEmployee);
 
 // Récupérer firstname et lastname de l'objet
 const firstname = myEmployee.firstname;
 const lastname = myEmployee.lastname;
 
-// Générer l'email
-const eMail = firstname+"."+lastname+"@example.com";
+
+
+const eMail = createMail(firstname, lastname);
 
 // Ajout de l'email dans le tableau
 employeeArray.splice(3, 0, eMail.toLowerCase());
@@ -75,7 +70,6 @@ const salary = myEmployee.salary + " €";
 employeeArray.splice(4, 1, salary);
 
 // Ajouter cellule et son texte pour chaque cellule du tbody
-console.log(employeeArray);
 employeeArray.forEach(element => {
     addCell(myRow, element);
 });
@@ -89,12 +83,58 @@ for (let index = 0; index < infoProfil.length; index++) {
     const lastN = myEmployee.lastname;
     infoProfil.splice(0, 1, firstN);
     infoProfil.splice(1, 1, lastN);
-    inputProfil[index].value = infoProfil[index];
-    
+    inputProfil[index].value = infoProfil[index]; 
 }
 
 // Contrôler les valeurs saisies dans le formulaire
+const btnSave = document.querySelector("#btn-save");
 
-// générer le nouveau email
+// const regexName = /^[A-Z]{0,1}[a-zàâäéèêëïîôöùûüÿç]{1,20}[-]{0,1}[a-zàâäéèêëïîôöùûüÿç]{0,20}$/;
+// const regexDate = /^[0-9]{2}-[0-9]{2}-[0-9]{4}$/;
+// const regexSalary = /^[0-9]{1,}$/;
+
+// if(!inputFirstname.match(regexName)){
+//     document.querySelector("#firstname").value = "";
+// }
+// if(!inputLastname.match(regexName)){
+//     document.querySelector("#lastname").value = "";
+// }
+// if(!Number.isNaN(new Date(...inputBirthDate).valueOf())){
+//     document.querySelector("#birth-date").value = "";
+//     console.log("date invalide");
+// }
 
 // Enregistrer les nouvelles valeurs dans le tableau
+btnSave.addEventListener('click', ()=>{
+
+    console.log(employeeArray);
+    console.log(myEmployee);
+    updateEmployee();
+});
+
+function updateEmployee(){
+    myEmployee.lastname = document.querySelector("#lastname").value;
+    myEmployee.firstname = document.querySelector("#firstname").value;
+    myEmployee.birthday = document.querySelector("#birth-date").value;
+    myEmployee.salary = document.querySelector("#salary").value;
+
+    // Renvoyer un tableau contenant les valeurs d'un objet
+    employeeArray = Object.values(myEmployee);
+    // Supprimer la row du tbody
+    myRow.remove();
+    // Créer le tr (dans le tbody)
+    myRow = myTBody.insertRow();
+    const eMail = createMail(myEmployee.firstname, myEmployee.lastname);
+
+    // Ajout de l'email dans le tableau
+    employeeArray.splice(3, 0, eMail.toLowerCase());
+
+    // Ajout du symbole monétaire pour salary
+    const salary = myEmployee.salary + " €";
+    employeeArray.splice(4, 1, salary);
+
+    // Ajouter cellule et son texte pour chaque cellule du tbody
+    employeeArray.forEach(element => {
+        addCell(myRow, element);
+    });
+}
