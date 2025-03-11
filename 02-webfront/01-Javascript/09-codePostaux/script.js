494189da68b3b1086a3c3e9994039e930655449c
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-03-11 05:05:21 +0100

    exo js 09-codePostaux afficher info ville

--- 02-webfront/01-Javascript/09-codePostaux/script.js
+++ 02-webfront/01-Javascript/09-codePostaux/script.js
@@ -1,18 +1,49 @@
+const search = document.querySelector("#zip-code");
 const validation = document.querySelector("#validation");
+const suggestions = document.querySelector("#suggestions");
+
+function displaySuggestions(){
+    const zipCode = document.querySelector("#zip-code").value;
+    if(zipCode.length >= 2){
+        townsList.forEach(element => {
+            if(zipCode == element.codePostal.substring(0,zipCode.length)){
+                const option = document.createElement("option");
+                option.setAttribute("class", "town");
+                option.value = `${element.codePostal} ${element.nomCommune}`;
+                suggestions.appendChild(option);
+            }
+        });
+    }
+    else{
+        const allSuggestions = document.querySelectorAll(".town");
+        allSuggestions.forEach(element =>{
+            element.remove();
+        });
+    }
+    return zipCode;
+}
+
 
 
 
 fetch("https://arfp.github.io/tp/web/javascript/02-zipcodes/zipcodes.json")
-.then(response => response.json())
-.then(data => {data.forEach(element => {
-        const inputCode = document.querySelector("#zip-code").value;
-        // substring()
-        if("59" === element.codePostal.substring(0,2)){
-            const listTowns = document.querySelector("#list-towns");
-            const option = document.createElement("option");
-            option.value = element.nomCommune;
-            listTowns.appendChild(option);
-            console.log(element);
-        }
-    })
+.then(response => {
+    return response.json();
+})
+.then((data) => {
+    townsList = data;
+    search.addEventListener('input', function displayInfo(){
+        const selection = displaySuggestions();
+
+        validation.addEventListener('click', function checkInput(){
+            console.log(selection.substring(6,));
+            // comparer la selection avec la liste
+            townsList.forEach(element => {
+                if(selection.substring(6,).match(element.nomCommune)){
+                    const infoTown = document.querySelector("#info-selection");
+                    infoTown.innerHTML = `ville : ${element.nomCommune} <br>code commune : ${element.codeCommune} <br>libelle d'acheminement : ${element.libelleAcheminement} <br>code postal ${element.codePostal}`;
+                }
+            });
+        });
+    });
 });
