8dc4a23c3b4c6f2fbda29e2761bd959156647fd1
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-03-12 16:48:10 +0100

    exo js 10 jeuCartes recupérer json dans tableau

--- 02-webfront/01-Javascript/10-jeuCartes/script.js
+++ 02-webfront/01-Javascript/10-jeuCartes/script.js
@@ -1,6 +1,7 @@
 // 
-const dataStats = document.querySelector("#data-stats"); 
-
+const myTBody = document.querySelector("#data-stats");
+const cellTitles = document.querySelectorAll("th");
+let tabResult = [];
 async function getCardGame(){
     const url = "https://arfp.github.io/tp/web/javascript/03-cardgame/cardgame.json";
     try{
@@ -11,29 +12,56 @@ async function getCardGame(){
             throw new Error(`Response status: ${response.status}`);
         }
         // Récupérer au format json
-        const json = await response.json();
-        //console.log(json);
-        return json;
+        tabResult = await response.json();
+        display(tabResult)
     }
     catch(error){
         // Afficher l'erreur dans la console
         console.error(error.message);
+        tabResult = []
     }
 }
 
-function addCell(myTRow){
-    myTRow.insertCell();
-}
+// function addCell(myTRow){
+//     myTRow.insertCell();
+// }
 
 // créer ligne tableau
-function addCard(myTBody){
-    const myTRow = myTBody.insertRow();
-    const statsCards = getCardGame();
-    console.log("stats : "+statsCards);
-    statsCards.forEach(card => {
-        const myCell = addCell(myTRow);
-        myCell.setAttribute()
-    });
+// function addCard(myTBody){
+//     const myTRow = myTBody.insertRow();
+//     const statsCards = getCardGame();
+//     console.log("stats : "+statsCards);
+//     statsCards.forEach(card => {
+//         const myCell = addCell(myTRow);
+//         myCell.setAttribute()
+//     });
+// }
+
+// addCard(dataStats);
+
+// Créer ligne
+const myTRow = myTBody.insertRow();
+
+// Récupérer json 
+//const statsCards = await getCardGame();
+//console.log(statsCards);
+
+
+// Initialiser les cellules du tableau
+// for (let i = 0; i < cellTitles.length; i++) {
+//     console.log(cellTitles[i].textContent);
+//     console.log(statsCards[i].name);
+    
+//     console.log(statsCards[i].filter(key => key == cellTitles[i].textContent));
+    
+// }
+const cellId = myTRow.insertCell();
+
+const display = (res) => {
+    res.forEach(r => {
+        console.log(r);
+        
+    })
 }
 
-addCard(dataStats);
+getCardGame()
