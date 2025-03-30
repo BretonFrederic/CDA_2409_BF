c2ca57738b76f1b544c073abd552259325c5c8c9
FBretonABCDEV <148881039+FBretonABCDEV@users.noreply.github.com>
2025-03-30 22:12:34 +0200

    exo js 01-Evaluation vueJS afficher stats de la classe et réussite examen oui/non

--- 02-webfront/02-Javascript/01b-Evalution-VueJS/script.js
+++ 02-webfront/02-Javascript/01b-Evalution-VueJS/script.js
@@ -1,7 +1,8 @@
 const appEvaluation = {
     data(){
         return{
-           listStudents: []
+           listStudents: [],
+           failinGrade: 12
         }
     },
     async created(){
@@ -10,13 +11,38 @@ const appEvaluation = {
         this.listStudents = jsonStudents.sort((a, b)=> b.grade - a.grade).map(student => ({
             ...student,
             lastname: student.fullname.split(" ")[0] || "",
-            firstname: student.fullname.split(" ")[1] || ""
+            firstname: student.fullname.split(" ")[1] || "",
+            success: this.examSuccess(student)
          }));
 
         // this.listStudents.forEach(student => {
         //    student["lastname"] = student.fullname.split(" ")[0];
         //    student["firstname"] = student.fullname.split(" ")[1];
         // }); 
+    },
+    computed:{
+        countStudents(){
+            return this.listStudents.length;
+        },
+        calculateGradeAvg(){
+            const sumGrades = this.listStudents.reduce((sum, student) => sum + student.grade, 0);
+            const gradeAverage = Math.round(sumGrades/this.listStudents.length);
+            return gradeAverage;
+        },
+        calculateAboveAvgGrades(){
+            const numberStudents = this.listStudents.filter(student => student.grade > this.calculateGradeAvg).length;
+            return numberStudents;
+        }
+    },
+    methods:{
+        examSuccess(currentStudent){
+            if(currentStudent.grade < this.failinGrade){
+                return "Non";
+            }
+            else{
+                return "Oui";
+            }
+        }
     }
 }
 
